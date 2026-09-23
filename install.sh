aa254180cc370b33533b29767dd0932252579d2f
aranchev <your-email@example.com>
2026-09-23 19:01:07 +0300

    a

--- install.sh
+++ install.sh
@@ -5,18 +5,19 @@ echo "Setting up dotfiles..."
 mkdir -p ~/.config/nvim ~/.config/tmux ~/.config/zsh
 
 link() {
-  target="$1"
-  dest="$2"
+    target="$1"
+    dest="$2"
 
-  if [ -e "$dest" ] || [ -L "$dest" ]; then
-    rm -rf "$dest"
-  fi
+    if [ -e "$dest" ] || [ -L "$dest" ]; then
+        rm -rf "$dest"
+    fi
 
-  ln -s "$target" "$dest"
+    ln -s "$target" "$dest"
 }
 
 link ~/dotfiles/nvim ~/.config/nvim
 link ~/dotfiles/tmux/tmux.conf ~/.config/tmux/tmux.conf
 link ~/dotfiles/zsh/.zshrc ~/.zshrc
+link ~/dotfiles/kitty ~/.config/kitty
 
 echo "Done."
